afd8cc41618f278c5d4be48628dc09e1563ae2cf
Ben Allen <benos.9k@gmail.com>
2015-05-20 20:08:33 +1200

    ...

--- ascii.py
+++ ascii.py
@@ -6,20 +6,47 @@ class AsciiRenderer:
 	
 	def __init__(self, gl):
 		self.gl = gl
+		
+		self.image_size = (0,0)
+		
+		# main FBO
+		self.fbo_main = GLuint(0)
+		self.tex_color = GLuint(0)
+		self.tex_depth = GLuint(0)
+		
+		# edge detection FBO
+		self.fbo_edge = GLuint(0)
+		self.tex_edge = GLuint(0)
+		
+		# ascii FBO
+		self.fbo_ascii = GLuint(0)
+		self.tex_ascii = GLuint(0)
+		
 	# }
 	
 	def render(self, w, h, game):
 		gl = self.gl
 		
-		# Clear the screen, and z-buffer
-		gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+		if (w, h) == (0, 0): return
+		
+		
+		
+		
+		# proper display
+		gl.glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
 		gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+		gl.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 		gl.glEnable(GL_DEPTH_TEST);
 		gl.glDepthFunc(GL_LESS);
 		gl.glViewport(0, 0, w, h);
 		
 		
 		
+		
+	# }
+	
+	def draw_text(self, row, col, text, color = (0.0, 0.0, 0.0)):
+		pass
 	# }
 	
 # }
