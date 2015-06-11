b2dee5ffee90267ccbcec6defa689195030c3de0
Ryan Sumner <ryan.j.sumner@gmail.com>
2015-06-11 14:25:03 +1200

    Highscore Finished!
    Highscore can now record an inputted 4 character name and a score
    (currently hardcoded because there is no score system) to the
    Highscore.txt.
    Then the scores are sorted and displayed on screen.

--- highscore.py
+++ highscore.py
@@ -29,12 +29,19 @@ class HighScoreState(object):
 		self.entries = []
 		for line in f:
 			line = re.sub('\n','',line)
-			print "line", line
 			string = re.split('\t', line)
-			print "string: ", string
 			self.entries.append(string)
 		f.close
-
+		self.state = 0
+		self.score = 7846
+		self.selected = 0
+		self.alphabet = map(chr, range(65, 91)) + [' ']
+		self.maxsize = len(self.alphabet)
+		self.pressed = False
+		self.delay = 0
+		self.name = ["","","",""]
+		self.selectindex = 0
+		self.selectlocation = 0.4
 
 
 	# Game logic
@@ -44,18 +51,92 @@ class HighScoreState(object):
 		if self.pause > 50:
 			if pressed[K_SPACE]:
 				return ArcadeMenuState()
-
-
+		if self.state==0:
+			if pressed[K_UP]:
+				if not self.pressed and self.delay <= 0:
+					self.selected += 1
+					self.selected %= self.maxsize
+				self.pressed = True
+				
+			elif pressed[K_DOWN]:
+				if not self.pressed and self.delay <= 0:
+					self.selected -= 1
+					self.selected %= self.maxsize
+				self.pressed = True
+				
+			elif pressed[K_RETURN]:
+				if not self.pressed and self.delay <= 0:
+					if self.selectindex < 4:
+						self.name[self.selectindex] = self.alphabet[self.selected]
+						self.selectindex += 1
+						self.selectlocation += 0.05
+						self.selected = 0
+				self.pressed = True
+				
+			elif pressed[K_DELETE]:
+				if not self.pressed and self.delay <= 0:
+					if self.selectindex > 0:
+						self.selectindex -= 1
+						self.name[self.selectindex] = ""
+						self.selectlocation -= 0.05
+						self.selected = 0
+				self.pressed = True
+				
+			else:
+				self.pressed = False
+			self.delay -= 1
+			
+			if self.selectindex >= 4:
+				#correct formatting
+				self.name = str(''.join(self.name))
+				self.entries.append([self.name,self.score])
+				for entry in self.entries:
+					entry[1] = int(entry[1])
+				
+				#sort entries
+				self.entries.sort(key=lambda tup: tup[1])
+				self.entries.reverse()
+				
+				#write to file
+				f = open(self.path,"w")
+				for entry in self.entries:
+					f.write( str(entry[0]) + str("\t") + str(entry[1]) + str('\n') )
+				
+				#reopen and reread
+				f = open(self.path,'r')
+				self.entries = []
+				for line in f:
+					line = re.sub('\n','',line)
+					string = re.split('\t', line)
+					self.entries.append(string)
+				self.state = 1
 
 
 	# Render logic
 	#
 	def render(self, gl, w, h, ascii_r=None):
-		if ascii_r:
+		if ascii_r and self.state==0:
+			titleArt = ascii.wordart('HIGHSCORE', 'big')
+			ascii_r.draw_text(titleArt, color = (0.333, 1, 1), screenorigin = (0.5, 0.9), textorigin = (0.5, 0.5), align = 'c')
+			
+			
+			selectedArt = ascii.wordart(self.alphabet[self.selected], 'big')
+			ascii_r.draw_text(selectedArt, color = (0.333, 1, 1), screenorigin = (self.selectlocation, 0.5), textorigin = (0.5, 0.5), align = 'c')
+			
+			for i in range(0,len(self.name)):
+				nameArt = ascii.wordart(self.name[i], 'big')
+				ascii_r.draw_text(nameArt, color = (0.333, 1, 1), screenorigin = (0.4+(0.05*i), 0.5), textorigin = (0.5, 0.5), align = 'c')
+			
+			
+			scoreArt = ascii.wordart(str(self.score), 'big')
+			ascii_r.draw_text(scoreArt, color = (0.333, 1, 1), screenorigin = (0.5, 0.1), textorigin = (0.5, 0.5), align = 'c')
+			
+			
+		if ascii_r and self.state==1:
 			#ascii_r.draw_text("Highscorse har plz!", color = (1, 1, 1), screenorigin = (0.5, 0.5), textorigin = (0.5, 0.5), align = 'c')
 			
-			art1 = ascii.wordart('HIGHSCORE', 'big')
-			ascii_r.draw_text(art1, color = (0.333, 1, 1), screenorigin = (0.5, 0.9), textorigin = (0.5, 0.5), align = 'c')
+			titleArt = ascii.wordart('HIGHSCORE', 'big')
+			ascii_r.draw_text(titleArt, color = (0.333, 1, 1), screenorigin = (0.5, 0.9), textorigin = (0.5, 0.5), align = 'c')
 			
 			count = 0
 			for entry in self.entries:
