a7d3f4d060b0ae5a24c96d9dca8e755900cb51c6
Ryan Sumner <ryan.j.sumner@gmail.com>
2015-06-15 12:36:26 +1200

    Highscore Input Logic + Chris

--- highscore.py
+++ highscore.py
@@ -34,6 +34,7 @@ class HighScoreState(object):
 		self.score = score
 		self.state = state
 		self.keyispressed = False
+		self.done = False
 		pygame.mixer.music.play(-1)
 
 	# Game logic
@@ -62,16 +63,22 @@ class HighScoreState(object):
 				elif controller.key_down(C_POS_X):
 					if self.selectindex < 4:
 						self.name[self.selectindex] = self.alphabet[self.selected]
-						self.selectindex += 1
-						self.selected = 0
+						if  self.selectindex < 3:
+							self.selected = self.alphabet.index(self.name[self.selectindex+1])
+							self.selectindex += 1
+						elif self.selectindex == 3:
+							self.done = True
 						self.delay = 10
 						self.keyispressed = True
 					
 				elif controller.key_down(C_NEG_X):
 					if self.selectindex > 0:
-						self.selectindex -= 1
-						self.name[self.selectindex] = "A"
-						self.selected = 0
+						if self.done:
+							self.done = False
+						else:
+							self.name[self.selectindex] = self.alphabet[self.selected]
+							self.selected = self.alphabet.index(self.name[self.selectindex-1])
+							self.selectindex -= 1
 						self.delay = 10
 						self.keyispressed = True
 				else:
@@ -111,50 +118,47 @@ class HighScoreState(object):
 	# Render logic
 	#
 	def render(self, gl, w, h, ascii_r=None):
-
-		titleArt = ascii.wordart('HIGHSCORE', 'star_wars')
-		ascii_r.draw_text(titleArt, color = (0.333, 1, 1), screenorigin = (0.5, 0.9), textorigin = (0.5, 0.5), align = 'c')
-			
-
-		if ascii_r and self.state==0:
-
-			if self.pause % 50 < 25 and self.selectindex >= 4:
-				selectedArt = ascii.wordart("[SHOOT] to continue", 'big')
-				ascii_r.draw_text(selectedArt, color = (0.333, 1, 1), screenorigin = (0.5, 0.3), textorigin = (0.5, 0.5), align = 'c')
+		if ascii_r:
+			if self.state==0:
+				titleArt = ascii.wordart('HIGHSCORE', 'star_wars')
+				ascii_r.draw_text(titleArt, color = (0.333, 1, 1), screenorigin = (0.5, 0.9), textorigin = (0.5, 0.5), align = 'c')
 				
-			elif (self.pause % 50 < 25 or self.keyispressed) and self.selectindex < 4:
-				character = self.alphabet[self.selected]
-				if character == " ":
-					character = "_"
-				selectedArt = ascii.wordart(character, 'big')
-				ascii_r.draw_text(selectedArt, color = (0.333, 1, 1), screenorigin = (self.startlocation+(self.selectindex*0.05), 0.5), textorigin = (0.5, 0.5), align = 'c')
-			
-			for i in range(0,len(self.name)):
-				if not i == self.selectindex:
-					character = self.name[i]
+				if self.pause % 50 < 25 and self.done:
+					selectedArt = ascii.wordart("[SHOOT] to continue", 'big')
+					ascii_r.draw_text(selectedArt, color = (0.333, 1, 1), screenorigin = (0.5, 0.3), textorigin = (0.5, 0.5), align = 'c')
+					
+				elif (self.pause % 50 < 25 or self.keyispressed) and not self.done:
+					character = self.alphabet[self.selected]
 					if character == " ":
 						character = "_"
-					nameArt = ascii.wordart(character, 'big')
-					ascii_r.draw_text(nameArt, color = (0.333, 1, 1), screenorigin = (self.startlocation+(0.05*i), 0.5), textorigin = (0.5, 0.5), align = 'c')
-			
-			
-			scoreArt = ascii.wordart("SCORE: "+str(self.score), 'big')
-			ascii_r.draw_text(scoreArt, color = (0.333, 1, 1), screenorigin = (0.5, 0.1), textorigin = (0.5, 0.5), align = 'c')
-			
-		if ascii_r and self.state>=1:
-
-			count = 0
-			for entry in self.entries:
-				nameArt = ascii.wordart(entry[0],'big')
-				ascii_r.draw_text(nameArt, color = (1, 0.333, 1), screenorigin = (0.25, 0.7-(0.1*count)), textorigin = (0.5, 0.5), align = 'l')
+					selectedArt = ascii.wordart(character, 'big')
+					ascii_r.draw_text(selectedArt, color = (0.333, 1, 1), screenorigin = (self.startlocation+(self.selectindex*0.05), 0.5), textorigin = (0.5, 0.5), align = 'c')
 				
-				scoreArt = ascii.wordart(entry[1],'big')
-				ascii_r.draw_text(scoreArt, color = (1, 0.333, 1), screenorigin = (0.75, 0.7-(0.1*count)), textorigin = (0.5, 0.5), align = 'r')
-				count+=1
-			if self.state==1:
-				yourscoreArt = ascii.wordart("YOUR SCORE: ",'big')
-				ascii_r.draw_text(yourscoreArt, color = (1, 0.333, 1), screenorigin = (0.5, 0.05), textorigin = (0.5, 0.5), align = 'c')
-				yourscoreNumArt = ascii.wordart(str(self.score),'big')
-				ascii_r.draw_text(yourscoreNumArt, color = (1, 0.333, 1), screenorigin = (0.75, 0.05), textorigin = (0.5, 0.5), align = 'r')
-			
+				for i in range(0,len(self.name)):
+					if not i == self.selectindex or self.done:
+						character = self.name[i]
+						if character == " ":
+							character = "_"
+						nameArt = ascii.wordart(character, 'big')
+						ascii_r.draw_text(nameArt, color = (0.333, 1, 1), screenorigin = (self.startlocation+(0.05*i), 0.5), textorigin = (0.5, 0.5), align = 'c')
+				
+				
+				scoreArt = ascii.wordart("SCORE: "+str(self.score), 'big')
+				ascii_r.draw_text(scoreArt, color = (0.333, 1, 1), screenorigin = (0.5, 0.1), textorigin = (0.5, 0.5), align = 'c')
+				
+			if self.state>=1:
+				count = 0
+				for entry in self.entries:
+					nameArt = ascii.wordart(entry[0],'big')
+					ascii_r.draw_text(nameArt, color = (1, 0.333, 1), screenorigin = (0.25, 0.7-(0.1*count)), textorigin = (0.5, 0.5), align = 'l')
+					
+					scoreArt = ascii.wordart(entry[1],'big')
+					ascii_r.draw_text(scoreArt, color = (1, 0.333, 1), screenorigin = (0.75, 0.7-(0.1*count)), textorigin = (0.5, 0.5), align = 'r')
+					count+=1
+				if self.state==1:
+					yourscoreArt = ascii.wordart("YOUR SCORE: ",'big')
+					ascii_r.draw_text(yourscoreArt, color = (1, 0.333, 1), screenorigin = (0.5, 0.05), textorigin = (0.5, 0.5), align = 'c')
+					yourscoreNumArt = ascii.wordart(str(self.score),'big')
+					ascii_r.draw_text(yourscoreNumArt, color = (1, 0.333, 1), screenorigin = (0.75, 0.05), textorigin = (0.5, 0.5), align = 'r')
+				
 
